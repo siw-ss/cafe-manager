4914b9015c538d7769279dbe0a0bdf5002f91696
siw-ss <siwarsoula@gmail.com>
2023-07-05 15:37:48 +0100

    minor changes

--- src/main/java/com/inn/cafe/restImpl/UserRestImpl.java
+++ src/main/java/com/inn/cafe/restImpl/UserRestImpl.java
@@ -6,11 +6,9 @@ import com.inn.cafe.service.UserService;
 import com.inn.cafe.utils.CafeUtils;
 
 import com.inn.cafe.wrapper.UserWrapper;
-import org.checkerframework.checker.optional.qual.Present;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
